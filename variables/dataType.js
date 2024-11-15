d47cca5ec9d09bd9354ce31fae1f19fa0f3bc6f3
choudharysneha1708 <choudharysneha1708@gmail.com>
2024-11-15 13:28:25 +0530

    data types in js

---
+++ variables/dataType.js
@@ -0,0 +1,15 @@
+"use strict";// treate all JS code as newer version
+// alert("hello!"); we are using nodejs not browser
+
+console.log(typeof undefined);
+console.log(typeof null);
+//primitive data type
+/**
+ * number => 2 to power53
+ * big int
+ * string =""
+ * boolean => true/false
+ * null => standalone value
+ * undefined =>
+ * symbol => unique
+ */
