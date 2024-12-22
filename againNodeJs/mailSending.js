8f1d5d00f60ecf84cdc966ece73ce30290f7ed27
choudharysneha1708 <choudharysneha1708@gmail.com>
2024-12-22 18:11:31 +0530

    send mail js

---
+++ againNodeJs/mailSending.js
@@ -0,0 +1,25 @@
+const { text } = require('express');
+var nodeMailer = require('nodemailer')
+var transport = nodeMailer.createTransport({
+    host:'smtp.gmail.com',
+    port:587,
+    secure:false,
+    requireTLS: true,
+    auth:{
+        user:'choudharysneha1708@gmail.com',
+        pass:'Kavita@1335'
+    }
+});
+var mailOptions={
+    from: "choudharysneha1708@gmail.com",
+    to: "choudharysneha1708@gmail.com",
+    subject: "Hello Mail",
+    text: "Hello Sneha!"
+}
+transport.sendMail(mailOptions, function(error, info){
+    if(error){
+      console.warn(error);
+    }else{
+        console.warn("email has been sent", info.response);
+    }
+})
